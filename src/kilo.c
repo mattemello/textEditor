2abe6f8309cbd2a121e2588b85e70ec4a7c61bcc
matteo <matteo.massimello@gmail.com>
2025-05-10 22:30:21 +0200

    feat: add some more

--- src/kilo.c
+++ src/kilo.c
@@ -1,15 +1,42 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios origin_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &origin_termios);
+}
+
 void enableRawMode() {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &origin_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = origin_termios;
 
     tcgetattr(STDIN_FILENO, &raw);
 
-    raw.c_lflag &= ~(ECHO);
-    
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
+
+    enableRawMode();
+
+    char c;
+
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        }else{
+            printf("%d ( %c )\n", c, c);
+        }
+    }
+
     return 0;
 }
